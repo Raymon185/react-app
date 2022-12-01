2008abf5546f9293a882f89bd0f692306f1047f8
Roman <raymon1852@gmail.com>
2022-12-01 13:50:57 +0300

    added new file

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,20 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>Dialogs</div>
-    )
-}
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Roman</div>
+                <div className={s.dialog}>Andrey</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Dimych</div>
+                <div className={s.dialog}>Jhon</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your first name</div>
+                <div className={s.message}>Yo</div>
+            </div>
+        </div>
+    );
+};
 export default Dialogs;
